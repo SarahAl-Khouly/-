50cb877a24d823be1440aa560522ea414b43fbd1
Sarah <talesandnovels15@gmail.com>
2025-04-30 15:14:10 +0300

    Add files via upload

---
+++ java/music-list.js
@@ -0,0 +1,26 @@
+let allMusic = [
+  {
+    name: "ياسكن النفس",
+    artist: "Dr.Mohamed Maghawry",
+    img: "images/5776033794355217710.jpg",
+    src: "music/song1.mp3"
+  },
+  {
+    name: "عروسة النور",
+    artist: "Muhammad Al-muqit",
+    img: "images/5803285585186638066.jpg",
+    src: "music/song2.mp3"
+  },
+  {
+    name: "DARKSIDE",
+    artist: "Neoni",
+    img: "images/5803285585186638110.jpg",
+    src: "music/song3.mp3"
+  },
+  {
+    name: "Im Still Here",
+    artist: "Sia",
+    img: "images/5803285585186638116.jpg",
+    src: "music/song4.mp3"
+  }
+];
